29cdf46f4220f50230811e30d7169f7c9d86aa36
Axel Huting <axhuting@xs4all.nl>
2019-04-04 18:56:45 +0200

    eerste functie in main.py

--- code/classes/cargo.py
+++
@@ -1,5 +0,0 @@
-class Cargo(object):
-    def __init__(id, mass, size):
-        self.id = id
-        self.mass = mass
-        self.size = size
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-#first and main document
+from cargo import *
+import numpy as np
+import pandas as pd
+
+
+def main():
+    print('hi')
+
+
+
+def cargoreader(csv):
+    "this function takes in a cargo csv file. and turns it into a"
+    df = pd.read_csv(csv)
+    print(df.head())
+    columnnames = []
+    for i in df:
+        columnnames.append(i)
+    parceldict = pd.DataFrame.to_dict(df)
+    fulldict = {}
+    for i in columnnames:
+        workdict = parceldict[i]
+        for x in workdict.keys():
+            if x in fulldict.keys():
+                currentlist = fulldict[x]
+                currentlist.append(workdict[x])
+            else:
+                fulldict[x] = [workdict[x]]
+    print(fulldict)
+cargoreader('CargoList1.csv')
