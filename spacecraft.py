f069b2cc5a51a1bcbead8ea4abc03cd0eee6d81a
Lotte Heek <lotteheek@wcw-stud-145-109-49-243.wireless.uva.nl>
2019-04-08 11:55:51 +0200

    spacecraft class

--- spacecraft.py
+++ spacecraft.py
@@ -1,1 +1,9 @@
-
+class Spacecraft(object):
+    def __init__(name, nation, payload, volume, mass, cost, ftw):
+        self.name = name
+        self.nation = nation
+        self.payload = payload
+        self.volume = volume
+        self.mass = mass
+        self.cost = cost
+        self.ftw = ftw
