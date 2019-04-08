7f5f20371dba26c68cbaea39247ebe68449d8210
Axel Huting <axhuting@xs4all.nl>
2019-04-08 12:48:40 +0200

    readers bestand toegevoegd

--- main.py
+++ main.py
@@ -1,41 +1,13 @@
 from cargo import *
 import numpy as np
 import pandas as pd
-
+from readers import cargoreader
+from readers import shipreader
 
 def main():
-    print('hi')
+    cargodict = cargoreader('CargoList1.csv')
+    shipdict = shipreader('SpaceCraft1.csv')
+    print(cargodict)
+    print(shipdict)
 
-
-
-def reader(cargocsv, shipcsv):
-    "this function takes in a cargo csv file. and turns it into a dictionary"
-    df = pd.read_csv(cargocsv)
-    fd = pd.read_csv(shipcsv)
-    columnnames = []
-
-    for i in df:
-        columnnames.append(i)
-
-    df['mw'] = df[columnnames[1]] / df[columnnames[2]]
-    df = df.sort_values(by = ['mw'], ascending = False)
-    columnnames.append('mw')
-    parceldict = pd.DataFrame.to_dict(df)
-    fulldict = {}
-    print(df.head())
-    for i in columnnames:
-        workdict = parceldict[i]
-
-        for x in workdict.keys():
-            if x in fulldict.keys():
-                currentlist = fulldict[x]
-                currentlist.append(workdict[x])
-            else:
-                fulldict[x] = [workdict[x]]
-
-    print(df.head())
-    print()
-    print(fd.head())
-
-
-reader('CargoList1.csv','SpaceCraft1.csv')
+main()
