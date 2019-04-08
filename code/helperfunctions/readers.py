4be4a94ac61e23f6813338eecf90077fe992d6a1
Axel Huting <axhuting@xs4all.nl>
2019-04-08 16:00:57 +0200

    structure

--- code/helperfunctions/readers.py
+++ code/helperfunctions/readers.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-from spacecraft import *
-from cargo import *
+from code.classes.spacecraft import *
+from code.classes.cargo import *
 
 def loadships(shipcsv):
     shipdict = shipreader(shipcsv)
--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-from data.cargo import *
+from code.classes.cargo import *
 import numpy as np
 import pandas as pd
 from code.helperfunctions.readers import cargoreader
 from code.helperfunctions.readers import shipreader
 
-<<<<<<< HEAD
+
 def main(cargocsv, shipcsv):
     parcellist = loadparcels(cargocsv)
     shiplist = loadships(shipcsv)
-    for i in parcellist:
-        print(i.mw)
-=======
-def main():
-    cargodict = cargoreader('CargoList1.csv')
-    shipdict = shipreader('SpaceCraft1.csv')
-    print(cargodict)
-    #print(shipdict)
->>>>>>> 27e55572e21a70e17bb258a8264733447f0b9837
+    print(parcellist)
+    print()
+    print()
+    print(shiplist)
 
 main('CargoList1.csv', 'SpaceCraft1.csv')
