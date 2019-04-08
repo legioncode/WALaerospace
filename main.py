438a1846828cb6e358b101ceb3c23b18621195c9
wytzz <wytzeaz@hotmail.com>
2019-04-08 15:45:16 +0200

    Please work

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import pandas as pd
 from readers import *
 from spacecraft import *
 
-def main():
+def main(cargocsv, shipcsv):
+    parcellist = loadparcels(cargocsv)
+    shiplist = loadships(shipcsv)
+    for i in parcellist:
+        print(i.mw)
 
-main()
+main('CargoList1.csv', 'SpaceCraft1.csv')
