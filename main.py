5e249ab0c6288efda57f03c8089a24999438f8dc
wytzz <wytzeaz@hotmail.com>
2019-04-09 12:54:28 +0200

    Small changes

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def main(cargocsv, shipcsv):
     parcellist = loadparcels(cargocsv)
     shiplist = loadships(shipcsv)
     workcraft = shiplist[0]
-    print([i.name for i in shiplist])
-    #print(possiblemovesA(shiplist, parcellist))
+    #print([i.name for i in shiplist])
+    print(possiblemovesA(shiplist, parcellist))
     #print(possiblemovesB(shiplist, parcellist))
     #print(possiblemovesC(shiplist, parcellist))
 main('data/CargoList1.csv', 'data/SpaceCraft1.csv')
