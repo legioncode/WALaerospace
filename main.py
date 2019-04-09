45d1ca00ad073fac8d6e8827bac20f45cbbf02d4
Axel Huting <axhuting@xs4all.nl>
2019-04-09 12:56:37 +0200

    MURGE

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import numpy as np
 import pandas as pd
 from code.helperfunctions.readers import loadparcels
 from code.helperfunctions.readers import loadships
-from possiblemoves import *
+from code.helperfunctions.possiblemoves import *
 
 def main(cargocsv, shipcsv):
     parcellist = loadparcels(cargocsv)
     shiplist = loadships(shipcsv)
     workcraft = shiplist[0]
     print([i.name for i in shiplist])
-    #print(possiblemovesA(shiplist, parcellist))
+    print(possiblemovesA(shiplist, parcellist))
     #print(possiblemovesB(shiplist, parcellist))
     #print(possiblemovesC(shiplist, parcellist))
 main('data/CargoList1.csv', 'data/SpaceCraft1.csv')
