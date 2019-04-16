c48c8546fd5fac158b350f8a9549fb425e8ae941
Lotte Heek <lotteheek@wcw-stud-145-109-11-81.wireless.uva.nl>
2019-04-16 13:44:10 +0200

    postnl update

--- code/algoritmes/postnl.py
+++ code/algoritmes/postnl.py
@@ -1,25 +1,24 @@
-from code.helperfunctions.readers import *
 from code.helperfunctions.possiblemoves import checkmove
 from code.helperfunctions.assign import assign
 
-def postnl(shiplist, parcellist):
-    # sort the parcellist based on mv ratio
+def sortparcels(parcellist):
     sorted_parcels = sorted(parcellist, key=lambda cargo: cargo.mw, reverse=False)
+    return sorted_parcels
+
+def sortspacecrafts(shiplist):
     sorted_ships = sorted(shiplist, key=lambda spacecraft: spacecraft.mw, reverse=False)
+    return sorted_ships
+
+def postnl(shiplist, parcellist):
+    # get sorted lists
+    sorted_parcels = sortparcels(parcellist)
+    sorted_ships = sortspacecrafts(shiplist)
+
     # create an extra list for remaining parcels
     remainders = []
 
-    # start iterating at half of list, append to spacecrafts 3 & 4
+    # start iterating back from half of list, append to spacecrafts 0 & 1
     half = len(sorted_parcels) / 2
-    for parcel in sorted_parcels[int(half):]:
-        if checkmove(parcel, sorted_ships[2]):
-            assign(sorted_ships[2], parcel)
-        elif checkmove(parcel, sorted_ships[3]):
-            assign(sorted_ships[3], parcel)
-        else:
-            remainders.append(parcel)
-
-    # start iterating back from half of list, append to spacecrafts 1 & 2
     worklist = sorted_parcels[0: int(half)]
     for parcel in range(len(worklist)):
         parcel -= (parcel+1)
@@ -30,9 +29,18 @@ def postnl(shiplist, parcellist):
         else:
             remainders.append(worklist[parcel])
 
+    # start iterating at half of list, append to spacecrafts 2 & 3
+    for parcel in sorted_parcels[int(half):]:
+        if checkmove(parcel, sorted_ships[2]):
+            assign(sorted_ships[2], parcel)
+        elif checkmove(parcel, sorted_ships[3]):
+            assign(sorted_ships[3], parcel)
+        else:
+            remainders.append(parcel)
+
     # prints
     print(f"remainders: {len(remainders)}")
-    print(f"space2: {sorted_ships[2].assigned}")
-    print(f"space3: {sorted_ships[3].assigned}")
-    print(f"space1: {sorted_ships[1].assigned}")
-    print(f"space0: {sorted_ships[0].assigned}")
+    print(f"space2: {len(sorted_ships[2].assigned)}")
+    print(f"space3: {len(sorted_ships[3].assigned)}")
+    print(f"space1: {len(sorted_ships[1].assigned)}")
+    print(f"space0: {len(sorted_ships[0].assigned)}")
