2e99ea8ccd7c5c0b91cd0f9e9e2057ac9d35368f
Lotte Heek <lotteheek@gmail.com>
2019-05-23 15:03:56 +0200

    helperfunctions

--- code/helperfunctions/mainhelper.py
+++ code/helperfunctions/mainhelper.py
@@ -5,13 +5,15 @@ import pickle
 
 
 def getProblem():
-    problem = input("Welcome to space freight! Choose problem 'a' or 'b': ").lower()
+    """This function prompts the user for which problem they want to solve and returns this problem"""
+    problem = input("Welcome to space freight! Do you want to optimize a: amount of parcels, or b: costs. Choose 'a' or 'b' ").lower()
     while problem not in ('a', 'b'):
         problem = input("Please choose 'a' or 'b': ").lower()
     return problem
 
 
 def getParcellist():
+    """This function prompts the user for which parcellist they want to use and returns this list"""
     parcels = int(input("Which list of packages do you want to use? Choose '1' or '2': "))
     while parcels not in (1, 2, 3):
         parcels = int(input("Please choose '1' or '2': "))
@@ -21,7 +23,8 @@ def getParcellist():
         return 'data/CargoList2.csv'
 
 
-def getAlgorithm():
+def getAlgorithmA():
+    """This function prompts the user for which algorithm they want to use and returns this"""
     algorithm = input(
         "What type of algorithm do you want to use? Choose 'random', 'greedy' or 'beamsearch': ").lower()
     while algorithm not in ('random', 'greedy', 'beamsearch'):
@@ -35,6 +38,7 @@ def getAlgorithm():
 
 
 def getHillclimber(shiplist, parcellist):
+    """This function prompts the user to choose whether they want to use a hillclimber or not and returns the results if so"""
     choice = input(
         "Do you want to try to bring more packages by running a hillclimber on this solution? Choose 'yes' or 'no': ").lower()
     while choice not in ('yes', 'no'):
@@ -44,3 +48,12 @@ def getHillclimber(shiplist, parcellist):
         packedships = pickle.load(open(solution, "rb"))
         visualpackages(packedships, 'hillclimber')
         massvolumeperc(packedships, 'hillclimber')
+
+def getConstraint():
+    """This function prompts the user for an additional constraint and returns the name of the appropriate algorithm"""
+    constraint = input(
+        "Do you want to make sure Trump's spacecrafts don't launch more often than Poetin's? Choose 'yes' or 'no': ").lower()
+    while constraint not in ('yes', 'no'):
+        constraint = input(
+            "Do you want to make sure Trump's spacecrafts don't launch more often than Poetin's? Choose 'yes' or 'no': ").lower()
+    return constraint
